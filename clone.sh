6a64b724fb6f0432b0170dba9fff5e477351a662
Benjamin Michalski <benjamin.michalski@gmail.com>
2015-04-07 19:16:27 +0200

    Dev.

--- clone.sh
+++ clone.sh
@@ -8,3 +8,12 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+cd $DIR
+
+git clone git@github.com:bmichalski/arlekin-core.git
+git clone git@github.com:bmichalski/arlekin-dbal.git
+git clone git@github.com:bmichalski/arlekin-dbal-driver-pdo-mysql.git
+git clone git@github.com:bmichalski/arlekin-dbal-migration.git
+git clone git@github.com:bmichalski/arlekin-dbal-migration-driver-pdo-mysql.git
+git clone git@github.com:bmichalski/arlekin-dml.git
+git clone git@github.com:bmichalski/arlekin-dml-driver-pdo-mysql.git
