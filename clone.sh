7a783f73ba4e2354d70aaa86ff0a15a52576dcff
Benjamin Michalski <benjamin.michalski@gmail.com>
2015-04-10 22:36:25 +0200

    Dev.

--- clone.sh
+++ clone.sh
@@ -17,3 +17,23 @@ git clone git@github.com:bmichalski/arlekin-dbal-migration.git
 git clone git@github.com:bmichalski/arlekin-dbal-migration-driver-pdo-mysql.git
 git clone git@github.com:bmichalski/arlekin-dml.git
 git clone git@github.com:bmichalski/arlekin-dml-driver-pdo-mysql.git
+git clone git@github.com:bmichalski/docker-arlekin-dev-sandbox.git
+
+cd docker-arlekin-dev-sandbox
+
+ARLEKIN_FOLDERS='arlekin-core 
+arlekin-dbal 
+arlekin-dbal-driver-pdo-mysql 
+arlekin-dbal-migration 
+arlekin-dbal-migration-driver-pdo-mysql 
+arlekin-dml 
+arlekin-dml-driver-pdo-mysql'
+
+mkdir -p arlekin
+
+cd arlekin
+
+for FOLDER in $ARLEKIN_FOLDERS
+do
+  ln -s ../../$FOLDER $FOLDER
+done
