265df8da54150797fded1bff80801c423cd83aea
Benjamin Michalski <benjamin.michalski@gmail.com>
2017-02-19 16:53:15 +0100

    Adds container-bash.sh.  Adds run.sh.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Get current file parent directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source $DIR/common.sh
+
+cd $DOCKER_DEV_REPOSITORY_FULLPATH && \
+export SSH_PORT=2222 && \
+export MYSQL_ROOT_PASSWORD=root && \
+docker-compose build && \
+docker-compose up
+
