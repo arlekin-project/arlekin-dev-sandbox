8436334e6fef5e31edf83315bd269c2bdc3f72b1
Benjamin Michalski <benjamin.michalski@gmail.com>
2017-02-19 16:26:16 +0100

    Removes project name. Adds info.

--- status.sh
+++ status.sh
@@ -2,15 +2,23 @@
 
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+echo ''
+echo 'Workspace status: '
+echo ''
 git status
 
 source $DIR/common.sh
 
-for REPOSITORY in $ARLEKIN_REPOSITORIES
+for REPOSITORY in $REPOSITORIES
 do
-  ARLEKIN_REPOSITORY_FULLPATH=$DIR/$REPOSITORY
-  echo "Changing directory: $ARLEKIN_REPOSITORY_FULLPATH"
-  cd $ARLEKIN_REPOSITORY_FULLPATH && git status
+  REPOSITORY_FULLPATH=$DIR/$REPOSITORY
+  echo ''
+  echo '======'
+  echo ''
+  echo "$REPOSITORY status: "
+  echo ''
+  echo "Changing directory: $REPOSITORY_FULLPATH"
+  cd $REPOSITORY_FULLPATH && git status
 done
 
 
